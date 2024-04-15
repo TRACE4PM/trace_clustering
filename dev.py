6bde0cf1260b9d0c7301bf6cac3c90c28ec16f27
Ania DAHACHE <ania.dahache@univ-lr.fr>
2024-04-15 10:42:56 +0200

    update clustering algo

---
+++ dev.py
@@ -0,0 +1,20 @@
+from trace_based_clust.algorithm import clustering_algo
+
+
+file_path = "/home/ania/Desktop/trace_clustering/services/clustering/test/result_res10k.csv"
+
+
+if __name__ == "__main__":
+    # clustering_algorithm = input("Enter clustering algorithm (DBSCAN/Agglomerative): ").strip()
+    clustering_algorithm = "Agglomerative"
+    if clustering_algorithm not in ['DBSCAN', 'Agglomerative']:
+        print("Invalid clustering algorithm.")
+    else:
+        algorithm_params = {}
+        if clustering_algorithm == 'DBSCAN':
+            algorithm_params['eps'] = float(input("Enter eps value for DBSCAN: "))
+            algorithm_params['samples'] = int(input("Enter min_samples value for DBSCAN: "))
+        elif clustering_algorithm == 'Agglomerative':
+            algorithm_params['nb'] = int(input("Enter number of clusters for Agglomerative: "))
+
+        print(clustering_algo(file_path, clustering_algorithm, algorithm_params))
--- trace_based_clust/algorithm.py
+++ trace_based_clust/algorithm.py
@@ -1,6 +1,11 @@
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import AgglomerativeClustering
-
+from sklearn.metrics import silhouette_score
+from .utils import save_clusters
+from sklearn.metrics import davies_bouldin_score
+import warnings
+import pandas as pd
+from distance.calc_dist import levenshtein
 
 def DBScan_clust(distance_matrix, params):
     cluster = DBSCAN(eps=params['eps'], min_samples=params['samples'] , metric='precomputed')
@@ -12,3 +17,29 @@ def Agglomerative_clust(distance_matrix, params):
     cluster = AgglomerativeClustering(n_clusters=params['nb'], linkage='single')
     cluster_assignments = cluster.fit_predict(distance_matrix)
     return cluster, cluster_assignments
+
+
+def clustering_algo(file_path, clustering_methode, params):
+
+    warnings.filterwarnings('ignore')
+
+    df = pd.read_csv(file_path, sep=";")
+
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    traces = df.groupby("client_id")["action"].apply(list).reset_index(name='trace')
+
+    distance_matrix = levenshtein(traces)
+
+    if clustering_methode =="DBSCAN":
+        clusters, cluster_assignement = DBScan_clust(distance_matrix, params)
+        silhouette = silhouette_score(distance_matrix, cluster_assignement)
+        return silhouette
+
+    elif clustering_methode == "Agglomerative":
+        clusters, cluster_assignement = Agglomerative_clust(distance_matrix, params)
+        db_score = davies_bouldin_score(distance_matrix, cluster_assignement)
+        silhouette = silhouette_score(distance_matrix, cluster_assignement)
+        return silhouette, db_score
+
+    save_clusters(df, cluster_assignement, traces)
